355ab91373ed0cc645f803d49e6f9a8ed926c1a6
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2025-09-10 14:38:04 +0530

    convenient running through dev and prod

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.NODE_ENV === 'development' ? '.env.local' : '.env'
+});
 const express = require("express");
 const connectDB = require("./config/db");
 const hierarchyRoutes = require("./routes/hierarchyRoutes");
